bb76c9e633cafd4951e49d0262cf0913b613fb18
Yeah.not <yeahhh.not@gmail.com>
2020-12-03 11:25:22 +0200

    Добавил мин сборку проекта в build: очистка, копирование img и html, less

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
 const gulp = require('gulp');
+const {series, parallel} = require('gulp');
+const del = require('del');
 
-function defaultTask(cb) {
-  console.log('This is the default gulp task - for test only')
-  cb();
+const less = require('gulp-less');
+
+function clean() {
+  return del('./build');
 }
 
-gulp.task('default', defaultTask);
+function styles() {
+  return gulp.src('./src/less/styles.less')
+         .pipe(less())
+         .pipe(gulp.dest('./build/css'));
+}
+
+function images() {
+  return gulp.src('./src/img/**/*')
+         .pipe(gulp.dest('./build/img'));
+}
+
+function html() {
+  return gulp.src('./src/*.html')
+         .pipe(gulp.dest('./build'));
+}
+
+let build = series(clean, parallel(styles, images, html));
+
+gulp.task('build', build);
