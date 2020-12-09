91be764331e7cfb0cb54be58e5fea8819fd6a836
Yeah.not <yeahhh.not@gmail.com>
2020-12-09 10:58:13 +0200

    Подключил доп плагины в сборку стилей

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const {series, parallel} = require('gulp');
 const del = require('del');
 
 const less = require('gulp-less');
+const sourcemaps = require('gulp-sourcemaps');
+const gcmq = require('gulp-group-css-media-queries');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 function clean() {
   return del('./build');
@@ -10,7 +14,15 @@ function clean() {
 
 function styles() {
   return gulp.src('./src/less/styles.less')
+         .pipe(sourcemaps.init())
          .pipe(less())
+         .pipe(gcmq())
+         .pipe(autoprefixer({
+           overrideBrowserslist: ['> 0.1%'],
+           cascade: false
+         }))
+         .pipe(cleanCSS({level: 2}))
+         .pipe(sourcemaps.write())
          .pipe(gulp.dest('./build/css'));
 }
 
