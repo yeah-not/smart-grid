d953204c4e7d24fbce26c30a8fe48756b68eb502
Yeah.not <yeahhh.not@gmail.com>
2020-12-10 10:42:24 +0200

    Подключил smart-grid и базово настроил под 24 колоночную верстку по макету. HTML Beautify.

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,15 @@ const gulpif = require('gulp-if');
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
+const isDev = (process.argv.indexOf('--dev') !== -1);
+const isProd = !isDev;
+
 const less = require('gulp-less');
 const sourcemaps = require('gulp-sourcemaps');
 const gcmq = require('gulp-group-css-media-queries');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-
-const isDev = (process.argv.indexOf('--dev') !== -1);
-const isProd = !isDev;
+const smartgrid = require('smart-grid');
 
 function clean() {
   return del('./build');
@@ -54,7 +55,38 @@ function watch() {
   gulp.watch('./src/*.html', html);
 }
 
+function grid(done) {
+  const settings = {
+    columns: 24,
+    offset: '10px',
+    // mobileFirst: true,
+    container: {
+        maxWidth: '950px',
+        fields: '30px'
+    },
+    breakPoints: {
+      md: {
+        width: "920px",
+        fields: "15px"
+      },
+      sm: {
+        width: "720px"
+      },
+      xs: {
+        width: "576px"
+      },
+      xxs: {
+        width: "420px"
+      }
+    }
+  };
+
+  smartgrid('./src/less', settings);
+  done();
+}
+
 let build = series(clean, parallel(styles, images, html));
 
 gulp.task('build', build);
 gulp.task('watch', series(build, watch));
+gulp.task('grid', grid);
